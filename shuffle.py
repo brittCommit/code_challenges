eb98783e582488ef932acfa742eac63bfd41041d
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-15 16:55:13 -0700

    added tests

--- shuffle.py
+++ shuffle.py
@@ -1,8 +1,17 @@
 def shuffle(nums, n):
     """Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
-        Return the array in the form [x1,y1,x2,y2,...,xn,yn]."""
-    
+    Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
+    >>> shuffle([2,5,1,3,4,7],3)
+    [2,3,5,4,1,7]
+
+    >>> shuffle([1,2,3,4,4,3,2,1], 4)
+    [1,4,2,3,3,2,4,1]
+
+    >>> shuffle([1,1,2,2], 2)
+    [1,2,1,2]
+    """
+    
     #find length of array/2 = n
     #create empty list for output
     #take first element of original list
@@ -22,3 +31,8 @@ def shuffle(nums, n):
         nums.pop(0)
         y_idx -= 1
     return output
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
