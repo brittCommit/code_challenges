bf7a18f85682fafa0dce988bf51b0eb50df15602
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-15 16:57:01 -0700

    fixed tests

--- shuffle.py
+++ shuffle.py
@@ -2,14 +2,14 @@ def shuffle(nums, n):
     """Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
     Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
-    >>> shuffle([2,5,1,3,4,7],3)
-    [2,3,5,4,1,7]
+    >>> shuffle([2, 5, 1, 3, 4, 7],3)
+    [2, 3, 5, 4, 1, 7]
 
-    >>> shuffle([1,2,3,4,4,3,2,1], 4)
-    [1,4,2,3,3,2,4,1]
+    >>> shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4)
+    [1, 4, 2, 3, 3, 2, 4, 1]
 
-    >>> shuffle([1,1,2,2], 2)
-    [1,2,1,2]
+    >>> shuffle([1, 1, 2, 2], 2)
+    [1, 2, 1, 2]
     """
     
     #find length of array/2 = n
