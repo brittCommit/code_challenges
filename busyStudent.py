9a21b760939035d5882f01b40b295312aec5a92d
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-15 16:46:50 -0700

    fixed errors in testing code

--- busyStudent.py
+++ busyStudent.py
@@ -1,24 +1,22 @@
-def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int):
-    """Return the number of students that were working during the queryTime.
+"""Return the number of students that were working during the queryTime.
 
-    >>>busyStudent([9,8,7,6,5,4,3,2,1],[10,10,10,10,10,10,10,10,10],10)
+    >>> busyStudent([9,8,7,6,5,4,3,2,1],[10,10,10,10,10,10,10,10,10],10)
     9
 
-    >>>busyStudent([1,1,1,1], [1,3,2,4], 7)
+    >>> busyStudent([1,1,1,1], [1,3,2,4], 7)
     0
 
-    >>>busyStudent([1,2,3], [3,2,7], 4)
+    >>> busyStudent([1,2,3], [3,2,7], 4)
     1
-    """
-        
+"""
+
+def busyStudent(startTime, endTime, queryTime):    
         
     #for each student in start time, check if value is < > query time
     #add 1 to return value if true
     # i = index
 
-    i = 0[9,8,7,6,5,4,3,2,1]
-[10,10,10,10,10,10,10,10,10]
-10
+    i = 0
 
     output = 0
     
