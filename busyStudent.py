645986cf286c7ec717601af34a7a22417f12611c
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-15 16:41:34 -0700

    completed challenge with added tests

---
+++ busyStudent.py
@@ -0,0 +1,35 @@
+def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int):
+    """Return the number of students that were working during the queryTime.
+
+    >>>busyStudent([9,8,7,6,5,4,3,2,1],[10,10,10,10,10,10,10,10,10],10)
+    9
+
+    >>>busyStudent([1,1,1,1], [1,3,2,4], 7)
+    0
+
+    >>>busyStudent([1,2,3], [3,2,7], 4)
+    1
+    """
+        
+        
+    #for each student in start time, check if value is < > query time
+    #add 1 to return value if true
+    # i = index
+
+    i = 0[9,8,7,6,5,4,3,2,1]
+[10,10,10,10,10,10,10,10,10]
+10
+
+    output = 0
+    
+    while i <= len(startTime) - 1:
+        if queryTime in range(startTime[i], endTime[i]+1):
+            output += 1
+        i += 1
+        
+    return output
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
