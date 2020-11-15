3a73e57929cafdce51b8be95d2af109300b070eb
Brittney Huttner <brittneyhuttner@gmail.com>
2020-11-14 20:54:20 -0800

    added smallest subarray

---
+++ sliding_window_practice.py
@@ -0,0 +1,27 @@
+import math
+def smallest_subarray_with_given_sum(s, arr):
+    """Given an array of positive numbers and a positive number ‘S,’ find the 
+    length of the smallest contiguous subarray whose sum is greater than or equal 
+    to ‘S’. Return 0 if no such subarray exists.
+    >>> smallest_subarray_with_given_sum([2, 1, 5, 2, 3, 2],7)
+    2
+    >>> smallest_subarray_with_given_sum([3, 4, 1, 1, 6], 8)
+    3
+    """
+  window_sum, window_start = 0, 0
+  min_length = math.inf
+
+  for window_end in range(len(arr)):
+    window_sum += arr[window_end]
+    while window_sum >= s:
+      min_length = min(min_length, window_end - window_start + 1)
+      window_sum -= arr[window_start]
+      window_start += 1
+  if min_length == math.inf:
+    return 0    
+  return min_length
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
