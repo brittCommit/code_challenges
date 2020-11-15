bf47b2ffe9acf5150a2fbb74f2494330ff03d677
Brittney Huttner <brittneyhuttner@gmail.com>
2020-11-14 21:43:13 -0800

    longest substring

--- sliding_window_practice.py
+++ sliding_window_practice.py
@@ -41,6 +41,33 @@ def max_sub_array_of_size_k(k, arr):
             w_start += 1
     return max_sum
 
+def longest_substring_with_k_distinct(str, k):
+    """
+    Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+    >>> longest_substring_with_k_distinct("araaci", 2)
+    4
+    >>> longest_substring_with_k_distinct("cbbebi", 3)
+    5
+    """
+    w_start, max_length = 0,0
+    char_frequency = {}
+
+    for w_end in range(len(str)):
+        right_char = str[w_end]
+        if right_char not in char_frequency:
+            char_frequency[right_char] = 0
+        char_frequency[right_char] += 1
+
+        while len(char_frequency) > k:
+            left_char = str[w_start]
+            char_frequency[left_char] -= 1
+            if char_frequency[left_char] == 0:
+                del char_frequency[left_char]
+            w_start += 1
+        max_length = max(max_length, w_end-w_start +1)
+    return max_length
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
